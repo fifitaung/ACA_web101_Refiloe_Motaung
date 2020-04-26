bd11fa9b5f670d2e61238db7aacc982e9708c962
User <user@MacBook-Pro.local>
2020-04-26 00:00:55 -0500

    hidden

--- Resume/resume-interaction.js
+++ Resume/resume-interaction.js
@@ -1,3 +1,11 @@
-function myFunction(){
-    document.getElementById("image").style.visibility = "hidden";
+function myImage(){
+    document.getElementById("image");
+    image.style.visibility = "hidden";
+}
+
+
+
+function myNum (){
+    document.getElementById("num");
+    num.style.visibility = "hidden";
 }
